7812c9d65013e30f70d24bbd3c7197b0595e540b
AliVeli7 <6gmrj9e@code.edu.az>
2022-04-01 08:16:15 +0400

    Created Hometask File

---
+++ ArrayConstructor/ArrayConstructor/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayConstructor
+{
+    class Product
+    {
+        public int No;
+        public string Name;
+        public double Price;
+        private int Count { get; set; }
+        public static int _count=0;
+
+        public Product()
+        {
+            
+
+            Console.WriteLine("No daxil et");
+            No = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name daxil et");
+            Name = Console.ReadLine();
+            Console.WriteLine("Price daxil et");
+            Price = Convert.ToDouble(Console.ReadLine());
+            Count = _count++;
+
+
+
+
+            
+        }
+
+        
+
+    }
+}
